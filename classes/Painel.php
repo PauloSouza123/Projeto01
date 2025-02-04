c83566df134bc6a59310b6f388abd6a03d135241
PauloSouza123 <ios14paul@gmail.com>
2025-02-04 14:59:29 -0300

    Sistema Lembrar Login

--- classes/Painel.php
+++ classes/Painel.php
@@ -14,6 +14,7 @@
 
         public static function loggout(){
             session_destroy();
+            setcookie('lembrar','true',time() - 1, '/');
             header('Location: ' .INCLUDE_PATH_PAINEL);
             die();
         }
--- painel/login.php
+++ painel/login.php
@@ -1,3 +1,22 @@
+<?php
+    if(isset($_COOKIE['lembrar'])){
+        $user = $_COOKIE['user'];
+        $password = $_COOKIE['password'];
+        $sql = MySql::conectar()->prepare("SELECT * FROM `tb_admin.usuarios` WHERE user = ? AND password = ?");
+        $sql->execute(array($user,$password));
+        if($sql->rowCount() == 1){
+            $info = $sql->fetch();
+            $_SESSION['login'] = true;
+            $_SESSION['user'] = $user;
+            $_SESSION['password'] = $password;
+            $_SESSION['nome'] = $info['nome'];
+            $_SESSION['img'] = $info['img'];
+            $_SESSION['cargo'] = $info['cargo'];
+            header('Location: '.INCLUDE_PATH_PAINEL);
+            die();
+        }
+    }
+?>
 <!DOCTYPE html>
  <html lang="pt-br">
  <head>
@@ -30,6 +49,11 @@
                     $_SESSION['nome'] = $info['nome'];
                     $_SESSION['img'] = $info['img'];
                     $_SESSION['cargo'] = $info['cargo'];
+                    if(isset($_COOKIE['lembrar'])){
+                        setcookie('lembrar',true,time() + (60 * 60 * 24), '/');
+                        setcookie('user',$user,time() + (60 * 60 * 24), '/');
+                        setcookie('password',$password,time() + (60 * 60 * 24), '/');
+                    }
                     header('Location: '.INCLUDE_PATH_PAINEL);
                     die();
                 }else{
@@ -42,7 +66,13 @@
             <form method="post">
                 <input type="text" name="user" placeholder="Login" required/>
                 <input type="password" name="password" placeholder="Senha" required/>
-                <input type="submit" name="acao" value="Enviar"/>
+                <div class="box-wraper-single-login">
+                    <input type="submit" name="acao" value="Enviar"/>
+                </div>
+                <div class="box-wraper-single-login">
+                    <label>Lembrar-me</label>
+                    <input type="checkbox" name="lembrar"/>
+                </div>
             </form>
         </div>
     </div>
